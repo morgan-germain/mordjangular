985981d94e31e9c13847847cf43fe133fc05f53b
Morgan Germain <mgermain@genymobile.com>
2014-02-11 00:02:56 +0100

    [Client] variabilize awesomeThings

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -4,8 +4,8 @@ var clientApp = angular.module('clientAppControllers', []);
 
 clientApp.controller('ClientAppCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      {name: 'HTML5 Boilerplate', content: 'HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.'},
+      {name: 'AngularJS', content: 'AngularJS is a toolset for building the framework most suited to your application development.'},
+      {name: 'Karma', content: 'Spectacular Test Runner for JavaScript.'},
     ];
   });
--- client/test/spec/controllers/main.js
+++ client/test/spec/controllers/main.js
@@ -19,4 +19,12 @@ describe('Controller: ClientAppCtrl', function () {
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
+
+  it('should have a name and a content attached to each awesomeThing', function () {
+    console.info('Listing each awesomeThing');
+    scope.awesomeThings.forEach(function(awesomeThing) {
+      expect(awesomeThing.name).toBeDefined();
+      expect(awesomeThing.content).toBeDefined();
+    });
+  });
 });
