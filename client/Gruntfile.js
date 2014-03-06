a8be0333185793ef0a129df2a7203cdf8e785625
Morgan Germain <mgermain@genymobile.com>
2014-03-06 13:26:25 +0100

    [Client] simplify html livereloads

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -77,7 +77,8 @@ module.exports = function (grunt) {
           livereload: '<%= connect.options.livereload %>'
         },
         files: [
-          '<%= yeoman.app %>/**/*.html',
+          '<%= yeoman.app %>/views/**/*.html',
+          '<%= yeoman.app %>/*.html',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
