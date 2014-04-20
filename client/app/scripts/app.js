41395552df41d33fe25015444aa6f00a26b0192b
Morgan Germain <mgermain@genymobile.com>
2014-04-20 18:49:03 +0200

    [krearts] make navbar become a sidebar

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -12,38 +12,42 @@ var app = angular.module('mainApp', [
 ]);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'mainCtrl',
-        activetab: 'home'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'mainCtrl',
-        activetab: 'about'
-      })
-      .when('/artists', {
-        templateUrl: 'views/artists.html',
-        controller: 'mainCtrl',
-        activetab: 'artists'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'mainCtrl',
-        activetab: 'contact'
-      })
-      .when('/links', {
-        templateUrl: 'views/links.html',
-        controller: 'mainCtrl',
-        activetab: 'links'
-      })
-      .when('/services', {
-        templateUrl: 'views/services.html',
-        controller: 'mainCtrl',
-        activetab: 'services'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'mainCtrl',
+      activetab: 'home'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'mainCtrl',
+      activetab: 'about'
+    })
+    .when('/artists', {
+      templateUrl: 'views/artists.html',
+      controller: 'mainCtrl',
+      activetab: 'artists'
+    })
+    .when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: 'mainCtrl',
+      activetab: 'contact'
+    })
+    .when('/links', {
+      templateUrl: 'views/links.html',
+      controller: 'mainCtrl',
+      activetab: 'links'
+    })
+    .when('/services', {
+      templateUrl: 'views/services.html',
+      controller: 'mainCtrl',
+      activetab: 'services'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
+
+// Workaround for AngularJS issue where you can nest ng-view inside a ng-inglude
+// https://github.com/angular/angular.js/issues/1213
+app.run(['$route', angular.noop]);
