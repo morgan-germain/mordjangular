712346432b4f42506307d466f0678458318ac0b8
Morgan Germain <mgermain@genymobile.com>
2014-04-21 21:40:44 +0200

    [krearts] include a little mp3 player

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,7 +2,40 @@
 
 var mainModule = angular.module('mainModule', []);
 
-mainModule.controller('mainCtrl', ['$rootScope', '$route', function ($scope, $route) {
+// See https://github.com/zohararad/audio5js
+mainModule.factory('AudioService', function () {
+  var params = {
+    swf_path:'../../swf/audio5js.swf',
+    throw_errors:true,
+    format_time:true
+  };
+
+  var audio5js = new Audio5js(params);
+
+  return audio5js;
+});
+
+
+mainModule.controller('mainCtrl', ['$rootScope', '$route', 'AudioService',
+      function ($scope, $route, AudioService) {
   // Keep route scope to obtain active tab
   $scope.$route = $route;
+
+  //bind AudioService to scope
+  $scope.player = AudioService;
+  //Load the song, every event, class method and Instance attribute from audio5js are accessible from the template
+  $scope.player.load('http://danosongs.com/music/danosongs.com-orb-of-envisage.mp3');
+
+  $scope.isPlaying = false;
+
+  //example of event binding
+  $scope.player.on('timeupdate',function(){
+    $scope.$apply();
+  });
+
+  $scope.onPlayPause = function() {
+    $scope.player.playPause();
+    $scope.isPlaying = ! $scope.isPlaying;
+  };
+
 }]);
