b053d7e01288693a936a686a420fd8d5ec4052a5
Morgan Germain <mgermain@genymobile.com>
2014-05-17 17:30:12 +0200

    [krearts] moved partnership and emails

--- client/app/contact-form.php
+++ client/app/contact-form.php
@@ -4,7 +4,7 @@ $data = json_decode(file_get_contents("php://input"));
 $email = $data->email;
 $content = $data->content;
 
-$dest = "morgan.germain@gmail.com, jfcecilio@gmail.com, alfonsedroguer@hotmail.com, toxicazsid@gmail.com";
+$dest = "morgan.germain@gmail.com, jfcecilio@gmail.com, arcaide@hotmail.fr, toxicazsid@gmail.com";
 $subject = "Krearts - contact form";
 $header = "From: $email\r\n";
 $header.= "Reply-to: $email\r\n";
