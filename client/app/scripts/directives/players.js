73d719ac576e39d7550614348dd8a93808be396e
Morgan Germain <mgermain@genymobile.com>
2014-08-05 14:09:12 +0200

    [krearts] bind tracks properly

--- client/app/scripts/directives/players.js
+++ client/app/scripts/directives/players.js
@@ -7,24 +7,21 @@ app.directive('jplayer', function() {
   return {
     restrict: 'EA',
     templateUrl: 'views/subviews/audio-jplayer.html',
+    scope: {
+      tracks: '='
+    },
     link: function(scope, element, attrs) {
-
-      var tracks = JSON.parse(attrs.tracks);
-
-
-      element.ready(function() {
-        var myPlaylist = new jPlayerPlaylist({
-          jPlayer: "#jquery_jplayer_1",
-          cssSelectorAncestor: "#jp_container_1"
-        },
-        tracks, {
-          swfPath: "./bower_components/jplayer/jquery.jplayer",
-          supplied: "mp3",
-          wmode: "window",
-          smoothPlayBar: true,
-          keyEnabled: true
-        });
-
+      // Initialize player
+      var myPlaylist = new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+      },
+      scope.tracks, {
+        swfPath: "./bower_components/jplayer/jquery.jplayer",
+        supplied: "mp3",
+        wmode: "window",
+        smoothPlayBar: true,
+        keyEnabled: true
       });
 
     }
